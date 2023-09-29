d094d497416c52b126160c6ebdbf6ae296f79f8d
LeongWeiXin <wxleong.2020@scis.smu.edu.sg>
2023-09-29 13:18:28 +0800

    edit inventory page

--- backend/backend.py
+++ backend/backend.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+import json
 
 app = Flask(__name__)
 
@@ -9,6 +10,32 @@ def register():
     email = data["email"]
     pwd  = data["pwd"]
     return {"message": "Added", "code": 201, "data": data}, 201
+
+@app.get('/allFoodItem')
+def get_foodItems():
+    response = {}
+    response["data"] = [
+                        {
+                            "foodItem": "Bread",
+                            "expiryDate": "2023-09-30",
+                            "quantity": 2,
+                            "units": "loaf",
+                            "category": "Grains",
+                            "id": 1
+                        },
+                        {
+                            "foodItem": "HL Milk",
+                            "expiryDate": "2023-10-04",
+                            "quantity": 500,
+                            "units": "mililitres",
+                            "category": "Milk Product",
+                            "id": 2
+                        }
+                    ]
+    response["message"] = "success"
+    response["code"] = 200
+    
+    return response
 
 if __name__ == '__main__':
     app.run()
--- foodsaver/lib/inventory.dart
+++ foodsaver/lib/inventory.dart
@@ -1,12 +1,58 @@
 import 'package:flutter/material.dart';
 import 'addFoodItem.dart';
-
+import 'package:http/http.dart' as http;
 import 'login.dart';
+import "dart:convert";
+import 'package:intl/intl.dart';
 
-class Inventory extends StatelessWidget {
+class Food {
+  final int foodId;
+  final String foodItem;
+  final String expiryDate;
+  final int quantity;
+  final String units;
+  final String category;
+
+  const Food(
+      {required this.foodId,
+      required this.foodItem,
+      required this.expiryDate,
+      required this.quantity,
+      required this.units,
+      required this.category});
+}
+
+class Inventory extends StatefulWidget {
+  @override
+  State<Inventory> createState() => _InventoryState();
+
   const Inventory({super.key, required this.email});
-
   final String email;
+}
+
+class _InventoryState extends State<Inventory> {
+  Future<List<Food>> getRequest() async {
+    String url = "http://127.0.0.1:5000/allFoodItem";
+    final response = await http.get(Uri.parse(url));
+
+    var responseData = json.decode(response.body);
+
+    //Creating a list to store input data;
+    List<Food> inventoryList = [];
+    for (var indvFood in responseData["data"]) {
+      Food food = Food(
+          foodId: indvFood["id"],
+          foodItem: indvFood["foodItem"],
+          expiryDate: indvFood["expiryDate"],
+          quantity: indvFood["quantity"],
+          units: indvFood["units"],
+          category: indvFood["category"]);
+
+      //Adding user to the list.
+      inventoryList.add(food);
+    }
+    return inventoryList;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,11 +65,11 @@ class Inventory extends StatelessWidget {
                 children: [
                   Padding(
                       padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 16),
+                          horizontal: 20, vertical: 10),
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Text("Welcome \n" + email,
+                          Text("Welcome \n" + widget.email,
                               style: const TextStyle(
                                 color: Color(0xFF003B2B),
                                 fontSize: 24,
@@ -43,7 +89,7 @@ class Inventory extends StatelessWidget {
                       )),
                   Padding(
                       padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 16),
+                          horizontal: 20, vertical: 10),
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
@@ -69,7 +115,7 @@ class Inventory extends StatelessWidget {
                                     context,
                                     MaterialPageRoute(
                                         builder: (context) =>
-                                            AddFoodItem(email: email)));
+                                            AddFoodItem(email: widget.email)));
                               },
                             ))
                           ]),
@@ -78,6 +124,57 @@ class Inventory extends StatelessWidget {
                   Divider(
                     color: Color(0xFF007F5C),
                     thickness: 0.5,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10.0),
+                    child: FutureBuilder(
+                      future: getRequest(),
+                      builder: (BuildContext ctx, AsyncSnapshot snapshot) {
+                        if (snapshot.data == null) {
+                          return Container(
+                            child: Center(
+                              child: CircularProgressIndicator(),
+                            ),
+                          );
+                        } else {
+                          return ListView.builder(
+                            scrollDirection: Axis.vertical,
+                            shrinkWrap: true,
+                            itemCount: snapshot.data.length,
+                            itemBuilder: (context, index) {
+                              return Column(
+                                children: <Widget>[
+                                  ListTile(
+                                    title: Text(snapshot.data[index].foodItem),
+                                    subtitle: Text(
+                                      "Expiring on: " +
+                                          snapshot.data[index].expiryDate,
+                                      style: TextStyle(
+                                          color: DateTime.parse(snapshot
+                                                          .data[index]
+                                                          .expiryDate)
+                                                      .difference(DateTime
+                                                          .parse(DateFormat(
+                                                                  "yyyy-MM-dd")
+                                                              .format(DateTime
+                                                                  .now())))
+                                                      .inDays <=
+                                                  1
+                                              ? Color(0xFFD63434)
+                                              : Color(0xFF007F5C)),
+                                    ),
+                                  ),
+                                  Divider(
+                                    color: Color(0xFF007F5C),
+                                    thickness: 0.5,
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        }
+                      },
+                    ),
                   ),
                 ],
               ))),
